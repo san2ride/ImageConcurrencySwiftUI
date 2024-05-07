ff89615877a8f0c204bc6f7d6a0f19f4ce3ea0ce
Jason <sanchezjason@me.com>
2024-05-07 13:59:32 -0600

    swiftui/concurrency/image_quote_app- Constants and gitignore

---
+++ ImageQuotesConcurrency/ImageQuotesConcurrency/Utils/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  ImageQuotesConcurrency
+//
+//  Created by Jason Sanchez on 5/7/24.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Urls {
+        static func getRandomImageUrl() -> URL? {
+            return URL(string: "https://picsum.photos/200/300?uuid=\(UUID().uuidString)")
+        }
+        
+        static let randomQuoteUrl: URL? = URL(string: "https://api.quotable.io/random")
+    }
+}
